6d78a58fb395f7211e6e10b09cc25ad0daf87465
Corina Gomez <cmgomez90@unm.edu>
2023-03-22 22:27:27 -0600

    Exploring Dataset
    Analyzing data and creating distribution plots

--- Rain-Garden-Soil-Temperatures.R
+++ Rain-Garden-Soil-Temperatures.R
@@ -60,3 +60,40 @@ T1$date_time=as.POSIXct(T1$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST
 T2$date_time=as.POSIXct(T2$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST")
 
 
+#### explore data set size and structure ####
+
+#analyze and format C1
+head(C1)
+str(C1)
+
+#plots of temperature values
+plot(C1$date_time, C1$X6.inches)
+ggplot2::qplot(x=date_time, y=X6.inches, data=C1, geom="point")
+
+#group all files by months
+C1$month = lubridate::month(C1$date_time)
+C2$month = lubridate::month(C2$date_time)
+T1$month = lubridate::month(T1$date_time)
+T2$month = lubridate::month(T2$date_time)
+
+#rerun plot of C1
+ggplot2::qplot(x=month, y=X6.inches, data=C1, geom="point")
+
+ggplot2::qplot(x=X6.inches, data=C1, geom="histogram")+
+  facet_grid(.~1)
+
+#group C1 by days
+C1$day = lubridate::day(C1$date_time)
+
+#create average monthly values
+C1 %>% dplyr::group_by(month) %>% summarise(mean(X6.inches))
+C2 %>% dplyr::group_by(month) %>% summarise(mean(X6.inches))
+T1 %>% dplyr::group_by(month) %>% summarise(mean(X6.inches))
+T2 %>% dplyr::group_by(month) %>% summarise(mean(X6.inches))
+
+#plotting tibbles 
+DFlng$month <- factor(DFlng$month)
+ggplot(C1, aes(x = month, y = mean(X6.inches), group = NULL, color = NULL)) + geom_line()
+
+                      
+
