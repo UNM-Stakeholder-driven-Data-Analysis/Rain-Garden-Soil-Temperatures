36ac1ca55b3977c7449e233c4f99f8be709326b6
Corina Gomez <cmgomez90@unm.edu>
2023-03-22 20:50:26 -0600

    Format Date/Time
    Examined format of date/time in files and reformatted to 24 hour cycles.

--- Rain-Garden-Soil-Temperatures.R
+++ Rain-Garden-Soil-Temperatures.R
@@ -16,3 +16,47 @@ T1 = read.csv("~/Documents/Data Analysis/R/Southwest Urban Hydrology/Rain-Garden
 T2 = read.csv("~/Documents/Data Analysis/R/Southwest Urban Hydrology/Rain-Garden-Soil-Temperatures/inside data/T2_SoilTempData.csv", header = TRUE)
 
 
+#### examine date/time ####
+
+#examine date/time for C1
+class(C1$Measurement.Time)
+head(C1$Measurement.Time)
+tail(C1$Measurement.Time)
+View(C1)
+
+#examine date/time for C2
+class(C2$Measurement.Time)
+head(C2$Measurement.Time)
+tail(C2$Measurement.Time)
+View(C2)
+
+#examine date/time for T1
+class(T1$Measurement.Time)
+head(T1$Measurement.Time)
+tail(T1$Measurement.Time)
+View(T1)
+
+#examine date/time for T2
+class(T2$Measurement.Time)
+head(T2$Measurement.Time)
+tail(T2$Measurement.Time)
+View(T2)
+
+
+#date/time column is currently in "%m/%d/%Y %H:%M %p" format
+
+#### reformat date/time ####
+
+# view formats
+?strptime
+
+#view timezones
+OlsonNames()
+
+#create new date/time format
+C1$date_time=as.POSIXct(C1$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST")
+C2$date_time=as.POSIXct(C2$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST")
+T1$date_time=as.POSIXct(T1$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST")
+T2$date_time=as.POSIXct(T2$Measurement.Time, format="%m/%d/%Y %I:%M %p", tz="MST")
+
+
